592e43b1345bd7cf6d22698d92c1c43c8845cae7
limingzhi-95 <17865923550@163.com>
2019-08-01 23:10:15 +0800

    add base admin

--- my_blog/comment/models.py
+++ my_blog/comment/models.py
@@ -20,5 +20,8 @@ class Comment(models.Model):
 
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
+    def __str__(self):
+        return self.content[:10]
+
     class Meta:
         verbose_name = verbose_name_plural = '评论'
